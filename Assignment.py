c0720c545f1feddf9d9cc7b2da67514e4d42fbc6
Hopplers <108778313+Hopplers@users.noreply.github.com>
2022-07-22 09:31:54 +0800

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,1 +1,7 @@
-
+def mainmenu
+  data = []
+  while flag == true:
+    option = (" Please select an option\n1. Add New Data\n2. Edit Existing Data\n3. Search Data\n4. View All Data\n5.Exit")
+    if option == 1
+    newdata()
+    
