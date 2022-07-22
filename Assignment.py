b91dc099a5cf6b0c005c6f1382821ba2d16bb485
Hopplers <108778313+Hopplers@users.noreply.github.com>
2022-07-22 09:58:53 +0800

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,4 +1,11 @@
-def mainmenu
+def newdata():
+  tname = input("Enter Tenent's Name")
+  tid = input("Enter Tenent's ID Number")
+  tbirthplace = input("Enter Tenent's Place of Birth")
+  tbirthcity = input("Enter Tenent's City of Birth")
+  tworkhis = input("Enter Tenent's Work History")
+  tworkemp = input("Enter Tenet's Work Employee")
+def mainmenu():
   data = []
   while flag == true:
     option = (" Please select an option\n1. Add New Data\n2. Edit Existing Data\n3. Search Data\n4. View All Data\n5.Exit")
